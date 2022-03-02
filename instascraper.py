d9f59585f93f3a394a27331ce6275fb3853dc0d7
morehwachege <antony123muriithi@gmail.com>
2022-03-02 17:38:43 +0300

    notta low

--- instascraper.py
+++ instascraper.py
@@ -1,6 +1,7 @@
-from instascrape import *
-acc = Profile('https://www.instagram.com/muriithigakuru_official/')
-acc_post = Post('https://www.instagram.com/p/CQyM5ZWFALg/')
-acc.scrape()
-acc_post.scrape()
-print(acc.followers)
+# import pandas as pd
+# import time
+# from selenium.webdriver import Chrome
+
+# browser = Chrome()
+# url = "https://www.instagram.com/"
+# browser.get(url)
