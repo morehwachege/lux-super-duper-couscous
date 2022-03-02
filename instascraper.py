91803cf388a543010e65db54d958e41acceb053c
morehwachege <antony123muriithi@gmail.com>
2022-03-03 00:38:27 +0300

    dead end

--- instascraper.py
+++ instascraper.py
@@ -1,7 +1,28 @@
-# import pandas as pd
-# import time
-# from selenium.webdriver import Chrome
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import os
+import wget
+from webdriver_manager.chrome import ChromeDriverManager
 
-# browser = Chrome()
-# url = "https://www.instagram.com/"
-# browser.get(url)
+
+options = Options()
+options.add_argument("no-sandbox")
+options.add_argument("start-maximized")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# driver = webdriver.Chrome("C:\chromedriver.exe")
+driver.get("https://facebook.com")
+# driver.get("https://instagram.com")
+# username = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+# password = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+
+# username.clear()
+# password.clear()
+# username.send_keys("black.katie_")
+# password.send_keys("moreh4aladin")
+
+# log_in = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
